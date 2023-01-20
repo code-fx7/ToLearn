a557e911d527b916698464a059d36b6fba8db9c6
code-fx <code-fx@mail.de>
2023-01-20 12:56:17 +0100

    add 06_SwitchCaseStatement.php in PHP

---
+++ PHP/Bootstrap-Academy/06_SwitchCaseStatement.php
@@ -0,0 +1,12 @@
+<?php
+$test_txt = "Muscheln";
+switch($test_txt) {
+    case "Spaghetti";
+        echo "Dein Lieblingsessen sind also Spaghetti.";
+        break;
+    case "Muscheln";
+        echo "Dein Lieblingsessen sind also Muscheln.";
+        break;
+    default:
+        echo "DeinLieblingsessen ist also irgendwas anderes.";    
+}
