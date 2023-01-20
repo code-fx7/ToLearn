3a6f027bb16fc606ed30a4d75135ee81ab1497aa
code-fx <code-fx@mail.de>
2023-01-20 12:28:59 +0100

    add 05_If-Bedingungen.php in PHP

---
+++ PHP/Bootstrap-Academy/05_If-Bedingungen.php
@@ -0,0 +1,11 @@
+<?php
+    $test_txt = "Hello World!";
+    $bedingung = true;
+    $zahl = 1337.42;
+    if($zahl > 1337) {
+        echo $zahl;
+    } else if ($bedingung) {
+        echo "ich bin immer wahr";
+    } else {
+        echo "zu kleine Zahl";
+}
