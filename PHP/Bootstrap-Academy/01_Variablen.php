f9bd6bd6875fb16c084185344da0f7941f255c81
code-fx <code-fx@mail.de>
2023-01-20 13:20:22 +0100

    edit 01_Variablen.php in PHP

--- PHP/Bootstrap-Academy/01_Variablen.php
+++ PHP/Bootstrap-Academy/01_Variablen.php
@@ -1,4 +1,4 @@
-<!-- Variablen beginnen mit einem $ gefolgt von einem Buchstaben groß oder klein oder einem Bodenstrich. Zulässige Zeichen sind a-z, A-Z, 0-9, _. Ausgabe im Browser mit echo. Mit echo kann man auch HTML ausgeben. -->
+<!-- Variablen beginnen mit einem $ gefolgt von einem Buchstaben groß oder klein oder einem Bodenstrich. Zulässige Zeichen sind a-z, A-Z, 0-9, _. Ausgabe im Browser mit echo. Mit echo kann man auch HTML ausgeben. Mit einem . lassen sich Strings verbinden.-->
 <?php
     $test_txt = "Hello World!";
     $x = 42;
