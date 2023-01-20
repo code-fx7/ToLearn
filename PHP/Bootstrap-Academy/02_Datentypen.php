52241da24f747fa88f12b59c121e56e2def7d15d
code-fx <code-fx@mail.de>
2023-01-20 11:58:27 +0100

    add 02_Datentypen in PHP

---
+++ PHP/Bootstrap-Academy/02_Datentypen.php
@@ -0,0 +1,7 @@
+<!-- var_dump() erhält man den Typ einer Variablen zurück und gibt diese aus. In einer Variablen kann man true und false speichern.-->
+
+<?php
+$X = 42;       //integer
+$Y = 1337.42; //float oder doubles
+var_dump($X);
+var_dump($Y);
