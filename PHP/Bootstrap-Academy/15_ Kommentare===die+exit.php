3c57ecbe8c5d9f9b0fe560000e8effcea3fba2ff
code-fx <code-fx@mail.de>
2023-01-20 15:18:36 +0100

    add 15_ Kommentare===die+exit.php in PHP

---
+++ PHP/Bootstrap-Academy/15_ Kommentare===die+exit.php
@@ -0,0 +1,9 @@
+
+<?php
+    //Kommentar einzeilig
+    #Kommentar einzeilig
+    /*Kommentar mehrzeilig*/
+    // == normaler Vergleich sind die Werte gleich?
+    // === Identitätsoperator muss auch den gleichen Typ haben
+    //die(); ab hier wird die Ausgabe abgebrochen
+    //exit(); ab hier wird die Ausgabe abgebrochen
