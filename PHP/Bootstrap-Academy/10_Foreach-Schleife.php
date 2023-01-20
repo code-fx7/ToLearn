3fa7b86442ad62a314e9420a91e61844a783e81e
code-fx <code-fx@mail.de>
2023-01-20 13:49:07 +0100

    add 10_Foreach-Schleife.php in PHP

---
+++ PHP/Bootstrap-Academy/10_Foreach-Schleife.php
@@ -0,0 +1,7 @@
+<!-- Mit as lassen sich Daten aus einem Array in eine Variable schreiben. -->
+
+<?php
+    $geschlechter = array("f", "m");
+    foreach ($geschlechter as $wert) {
+        echo $wert;
+    }
