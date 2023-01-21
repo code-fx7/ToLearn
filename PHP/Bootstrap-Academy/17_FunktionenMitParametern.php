329ade85c53058bd018357dcfee721b6843330fe
code-fx <code-fx@mail.de>
2023-01-21 14:16:31 +0100

    add 17_FunktionenMitParametern.php in PHP

---
+++ PHP/Bootstrap-Academy/17_FunktionenMitParametern.php
@@ -0,0 +1,15 @@
+<?php
+    function playGwent($name) {
+        echo "Bock auf eine Runde Gwent, $name ?";
+        echo "<p>Aber sicher doch!</p>";
+    }
+
+    //playGwent("Triss");
+    //$variable = "Ciri";
+    //playGwent($variable);
+
+    function addieren($a, $b) {
+        echo $a + $b;
+    }
+
+    addieren(5, 42);
