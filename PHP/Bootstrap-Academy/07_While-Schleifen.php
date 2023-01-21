46aa86e280dfa73b8af53d66c533b00fb38acf3d
code-fx <code-fx@mail.de>
2023-01-21 15:09:17 +0100

    edit 07_While-Schleifen.php in PHP

--- PHP/Bootstrap-Academy/07_While-Schleifen.php
+++ PHP/Bootstrap-Academy/07_While-Schleifen.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+<!-- Die Do-While-Schleife
+ -->    
 <?php
     $zahl = 1338;
     while($zahl < 1350) {
@@ -15,7 +7,3 @@
         echo "<br>";
         $zahl = $zahl +1;
     }
-?>
-
-</body>
-</html>
